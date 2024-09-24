b8b1c3eede9ab6af1dda5aeab2a45c802b2a9bb3
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-24 17:09:54 -0300

    Create retângulo.c

---
+++ retângulo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int retangulo(int m, int n){
+	int largura, altura;
+	
+	for(largura=1; largura <= m; largura++){
+		
+		for(altura=1; altura <= n; altura++){
+			
+			putchar((largura > 1 && largura < m && altura > 1 && altura < n) ? 000 : 003);
+		}
+		printf("\n");
+	}
+	return retangulo;
+}
+
+int main()
+{
+    int m, n;
+    printf("Digite a largura do retangulo: ");
+    scanf("%d", &m);
+    printf("Digite a altura do retanugol: ");
+    scanf("%d", &n);
+    
+    return retangulo(m, n);
+
+
+}
