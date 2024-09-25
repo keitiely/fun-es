fb09ca6ea9d11f285d31f177a41758c67632e472
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-24 22:26:31 -0300

    Update and rename sub-rotina to sub-rotina.c

--- sub-rotina.c
+++ sub-rotina.c
@@ -1,5 +1,6 @@
 /*1-Faça um programa contendo uma sub-rotina 
-que retorne 1 caso o número digitado seja positivo ou zero se negativo.*/
+que retorne 1 caso o número digitado seja positivo ou zero se negativo*/
+
 #include <stdio.h>
 
 int subrotina(int num)
