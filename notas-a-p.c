dd5496d957fbef801a75ce8d299fcb9895fc84bb
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-24 22:27:49 -0300

    Update notas-a-p.c

--- notas-a-p.c
+++ notas-a-p.c
@@ -1,3 +1,7 @@
+/*6-Elabore um programa contendo uma sub-rotina que receba três notas e uma letra como parâmetros.
+Se a letra for A, então deve ser retornada a média aritmética das notas; se a letra for P, 
+então deve ser retornada a média ponderada, com pesos 5, 3 e 2, respectivamente.
+A média calculada deverá ser devolvida ao programa principal para, então, ser mostrada.*/
 #include <stdio.h>
 
 int notas(float n1, float n2, float n3, char m){
